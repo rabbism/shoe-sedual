3d45c24a7bbaa375111b615a910ab5f1761d39bd
rabbism <alrabbism@gmail.com>
2022-05-04 10:27:36 -0700

    all done

--- src/component/Show/Show.js
+++ src/component/Show/Show.js
@@ -6,11 +6,13 @@ const Show = (props) => {
     return (
         <div class="col-6">
         <div class="p-3 border bg-light">
+            
             <h3>Name : {name}</h3>
             <h4>Language : {language}</h4>
             <Link to={`/show/${id}`}>
             <button className='btn btn-primary p-3 mt-2'>Details</button>
             </Link>
+            
             
             </div>
       </div>
